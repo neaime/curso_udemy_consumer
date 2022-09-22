73724834b7a07cc5c71c103540d7f668b3c2bdc0
neaime <fneaime@gmail.com>
2022-09-21 21:24:58 -0300

    Initial commit

---
+++ src/test/java/com/felipe/strconsumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.strconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
